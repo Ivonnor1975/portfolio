c96de35d4eb7d3463fc2a2be9162c3d67f7ee23a
Ivonne <ivonne@jjf.com>
2022-01-16 11:26:10 -0600

    add toggle nav bar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,21 +5,20 @@ import { NavLink } from 'react-router-dom'
 const Header = () => {
   return (
 
-    <nav className="navbar navbar-expand-lg nav">
-
-        <h1 className="p-4 ">IVONNE FERNANDEZ</h1>
+    <nav className="navbar navbar-expand-lg navbar-dark mb-2">
+        <h1 className="p-4">IVONNE FERNANDEZ</h1>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+                <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
+                <li className="nav-item fs-5 active">
                     <NavLink className="nav-link" aria-current="page" to="About">About Me</NavLink>
                 </li>                         
-                <li className="nav-item">
+                <li className="nav-item fs-5">
                       <NavLink className="nav-link" to="./Projects">Work</NavLink>                         
                 </li>
-                <li className="nav-item">
+                <li className="nav-item fs-5">
                     <NavLink className="nav-link" to="./ContactForm">Reach Out</NavLink>
                </li>
             </ul>
